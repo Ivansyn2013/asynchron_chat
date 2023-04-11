43eb12bb618cb8b531b7331dfa4a1879f3e29e09
ivan <ivansyn2013@yandex.ru>
2023-04-11 15:36:31 +0300

    lesson 3  start

--- lesson3/server.py
+++ lesson3/server.py
@@ -3,19 +3,36 @@ from socket import *
 import time
 from config import AppConfig
 from colorama import Fore
+import json
 
 
 def get_message(client):
-    pass
-def send_message(client):
-    pass
+    '''get message from client in serrialize it
+    :return responce'''
+    package = client.recv(AppConfig.APP_MAX_MESS_SIZE)
+    if isinstance(package, bytes):
+        json_responce = package.decode(AppConfig.APP_ENCODING)
+        responce = json.loads(json_responce)
+        if isinstance(responce, dict):
+            return responce
+        raise ValueError
+    raise ValueError
+
+
+def send_message(work_socket, message):
+    '''encoding and sending messages
+    :return'''
+    json_message = json.dumps(message)
+    package = json_message.encode(AppConfig.APP_ENCODING)
+    work_socket.send(package)
+
 
 
 def server_start():
-
+    '''Start servet app'''
     try:
         if '-a' in sys.argv:
-            IP = int(sys.argv[sys.argv.index('-a') + 1 ])
+            IP = int(sys.argv[sys.argv.index('-a') + 1])
         else:
             IP = AppConfig.APP_ADR
     except (IndexError, ValueError) as error:
@@ -23,7 +40,7 @@ def server_start():
 
     try:
         if '-p' in sys.argv:
-            PORT = int(sys.argv[sys.argv.index('-p') + 1 ])
+            PORT = int(sys.argv[sys.argv.index('-p') + 1])
             if not 1024 < PORT < 65535:
                 raise ValueError
         else:
@@ -40,7 +57,8 @@ def server_start():
         print(Fore.GREEN + 'App is started' + Fore.RESET)
         client, client_ip = server_socket.accept()
         time_str = time.ctime(time.time()) + "\n"
-        print(Fore.YELLOW + f'Connection from {client_ip} is accepted' + Fore.RESET)
+        print(
+            Fore.YELLOW + f'Connection from {client_ip} is accepted' + Fore.RESET)
         client.send(AppConfig.APP_WELCOME_MESS.encode(AppConfig.APP_ENCODING))
         client.send(time_str.encode('ascii'))
         client.close()
