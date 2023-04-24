14f0afeffe0d65b8465c0bdb66ee70ca54e64021
ivan <ivansyn2013@yandex.ru>
2023-04-25 00:54:30 +0300

    dz6 try

--- lesson6_dec/wrapper.py
+++ lesson6_dec/wrapper.py
@@ -1,9 +1,23 @@
-def log(func):
-    def wrapper(func, *args, **kwargs):
-        func = func
-    return func
+import logging
+import logs.client_log_config
+import logs.server_log_config
+from datetime import datetime
 
-return func
-
+from functools import wraps
+class Logs():
+    def __init__(self):
+        if self.__module__ == 'server.py':
+            self.logger = logging.getLogger('server')
+        else:
+            self.logger = logging.getLogger('client')
+    def __call__(self, func):
+        @wraps()
+        def wrapper(*args,**kwargs):
+            result = func(*args, **kwargs)
+            log_message = f'{datetime} Функция {func.__name__} Вызвана из ' \
+                          f'функции {func.__module__}'
+            self.logger.debug(log_message)
+            return result
+        return wrapper
 
 
